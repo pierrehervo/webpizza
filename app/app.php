0b22477f3651680aa09b30152e945aaf9291d486
PierreHervo <pierre.hervo@laposte.net>
2019-07-08 16:58:11 +0200

    -

---
+++ app/app.php
@@ -0,0 +1,1 @@
+mon appli est démarrée !
---
+++ public/index.php
@@ -0,0 +1,7 @@
+ <?php
+ /**
+  * Fichier  de démarrage de l'application
+  */
+
+  //Intégration du fichier d'éxecution de l'application
+  require_once"../app/app.php";
