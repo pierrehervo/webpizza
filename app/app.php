07001a5cf1d39c6905a6862f6891e666b7e6a882
PierreHervo <pierre.hervo@laposte.net>
2019-07-11 17:29:44 +0200

    -

--- app/app.php
+++ app/app.php
@@ -1,19 +1,38 @@
 <?php
 /**
- * Fichier d'éxecution de l'application
+ * Fichier d'exécution de l'application
  */
-
- /**
-  * 1.Démmarage de la session
-  */
-
-  /**
-   * 2.Intégration de la configuration
-   */
-  require_once "../config/config.php";
-
-
 /**
- * 3.Définition de l'environnement
+ * 1. Démarrage de la session
+ * --
+ * La session va permettre de suivre le visiteur pendant la durée de sa navigation
+ */
+/**
+ * 2. Intégration de la configuration
+ */
+require_once "../config/config.php";
+/**
+ * 3. Définition de l'environnement
  */
 require_once "../app/environnement.php";
+/**
+ * 4. Comportement des erreurs
+ */
+require_once "../app/err_reporting.php";
+/**
+ * 5. Connections aux base de données
+ */
+require_once "../app/dbconnect.php";
+/**
+ * 6. Routage de l'application
+ */
+require_once "../app/routing.php";
+/**
+ * 7. Inclusion des fonctions "Utils"
+ */
+/**
+ * 8. Compilation de la page (part 1 - Le controleur principal)
+ */
+/**
+ * 9. Compilation de la page (part 2 - la compilation finale)
+ */
